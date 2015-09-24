6fc5372604cbf73aa1fb80eb2ae2004006bfc4b7
adarshpandey <pandey.adarsh147@gmail.com>
2015-09-25 02:26:19 +0530

    f1 commit

--- app/src/main/java/me/syncify/gittest/MainActivity.java
+++ app/src/main/java/me/syncify/gittest/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        // f1 changes made here
     }
 
     @Override
